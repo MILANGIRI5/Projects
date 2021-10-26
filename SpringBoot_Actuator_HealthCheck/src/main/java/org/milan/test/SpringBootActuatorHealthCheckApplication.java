54e63ea91557b2c07d17e362ae12e2229368c3d2
MILANGIRI5 <milangiri50@gmail.com>
2021-10-27 00:30:30 +0530

    Project Adding FirstTime

---
+++ SpringBoot_Actuator_HealthCheck/src/main/java/org/milan/test/SpringBootActuatorHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package org.milan.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorHealthCheckApplication.class, args);
+	}
+
+}
